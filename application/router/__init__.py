d81f0551b41efcf081b7ff50b3167d3b35b79053
Trent Dierking <deerxlord@gmail.com>
2022-03-11 02:24:59 -0600

    cleanup

--- application/router/__init__.py
+++ application/router/__init__.py
@@ -1,3 +1,4 @@
+from collections import Iterable
 from types import ModuleType
 from typing import Generator, Iterator, Type
 
@@ -8,21 +9,19 @@ from sqlmodel import SQLModel
 from application import models
 from application.lib import database
 
+__all__ = ["generate_routers"]
+
 
 def generate_routers() -> Iterator[APIRouter]:
-    for package in packages():
-        pkg_name = package.__name__.replace("application.models.", "").lower()
-        router = APIRouter(prefix=f"/{pkg_name}")
-        for model in data_models(package):
-            crudrouter = SQLAlchemyCRUDRouter(
-                schema=model,
-                db_model=model,
-                db=db_conn,
-                prefix=f"/{model.__name__}",
-                tags=[f"{model.__name__}s"],
-            )
-            router.include_router(crudrouter)
-        yield router
+    return map(generate_router, packages())
+
+
+def generate_router(package: ModuleType) -> APIRouter:
+    pkg_name = package.__name__.replace("application.models.", "").lower()
+    router = APIRouter(prefix=f"/{pkg_name}")
+    crudrouters = map(crudrouter, data_models(package))
+    include_routers(router, crudrouters)
+    return router
 
 
 def packages() -> Iterator[ModuleType]:
@@ -36,6 +35,21 @@ def data_models(package: ModuleType) -> Iterator[Type[SQLModel]]:
         getattr(package, model) for model in dir(package) if not model.startswith("_")
     )
     return filter(lambda m: hasattr(m, "__table__"), trimmed)
+
+
+def include_routers(router: APIRouter, subrouters: Iterable[APIRouter]) -> None:
+    for subrouter in subrouters:
+        router.include_router(subrouter)
+
+
+def crudrouter(model: Type[SQLModel]) -> SQLAlchemyCRUDRouter:
+    return SQLAlchemyCRUDRouter(
+        schema=model,
+        db_model=model,
+        db=db_conn,
+        prefix=f"/{model.__name__}",
+        tags=[f"{model.__name__}s"],
+    )
 
 
 def db_conn() -> Generator[database.Session, None, None]:
