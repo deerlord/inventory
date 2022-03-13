cb3f60363b5b3b31f78a0b1deb4b1298a00f731e
Trent Dierking <deerxlord@gmail.com>
2022-03-12 23:28:15 -0600

    coverage

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -2,7 +2,7 @@ import os
 import random
 
 import pytest
-from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
 from application import setup_application
 
@@ -13,70 +13,102 @@ ENDPOINT = "/food/ingredient"
 def client():
     os.environ["DEBUG"] = "TRUE"
     app = setup_application()
-    yield TestClient(app)
+    yield AsyncClient(app=app, base_url="http://localhost:8000")
     os.remove("tests/data.sqlite")
 
 
-def test_get_all_but_none(client):
-    response = client.get(ENDPOINT)
+@pytest.mark.asyncio
+async def test_get_all_empty(client):
+    response = await client.get(ENDPOINT)
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_create(client):
+@pytest.mark.asyncio
+async def test_get_all(client):
+    data = await make_several(client)
+    response = await client.get(ENDPOINT)
+    assert response.status_code == 200
+    assert response.json() == data
+
+
+@pytest.mark.asyncio
+async def test_get_one_not_found(client):
+    response = await client.get(f"{ENDPOINT}/1")
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_create(client):
     expected_id = 1
     expected_name = "garlic"
     expected_model = {"id": expected_id, "name": expected_name}
-    response = client.post(ENDPOINT, json={"name": expected_name})
+    response = await client.post(ENDPOINT, json={"name": expected_name})
     assert response.status_code == 200
     assert response.json() == expected_model
-    response = client.get(ENDPOINT)
+    response = await client.get(ENDPOINT)
     assert response.status_code == 200
     assert response.json() == [expected_model]
-    response = client.get(f"{ENDPOINT}/{expected_id}")
+    response = await client.get(f"{ENDPOINT}/{expected_id}")
     assert response.status_code == 200
     assert response.json() == expected_model
 
 
-def test_create_many(client):
-    data = make_several(client)
+@pytest.mark.asyncio
+async def test_create_many(client):
+    data = await make_several(client)
     item_id = random.randrange(1, len(data))
-    response = client.get(f"{ENDPOINT}/{item_id}")
+    response = await client.get(f"{ENDPOINT}/{item_id}")
     assert response.status_code == 200
     assert response.json() == data[item_id - 1]
 
 
-def test_delete_one(client):
-    data = make_several(client)
+@pytest.mark.asyncio
+async def test_delete_one(client):
+    data = await make_several(client)
     item_id = random.randrange(1, len(data))
-    response = client.delete(f"{ENDPOINT}/{item_id}")
+    response = await client.delete(f"{ENDPOINT}/{item_id}")
     expected_delete = data.pop(item_id - 1)
     assert response.status_code == 200
     assert response.json() == expected_delete
-    response = client.get(ENDPOINT)
+    response = await client.get(ENDPOINT)
     assert response.status_code == 200
     assert response.json() == data
-    response = client.get(f"{ENDPOINT}/{item_id}")
+    response = await client.get(f"{ENDPOINT}/{item_id}")
     assert response.status_code == 404
 
 
-def test_delete_all(client):
-    make_several(client)
+@pytest.mark.asyncio
+async def test_delete_all(client):
+    await make_several(client)
     for method in (client.delete, client.get):
-        response = method(ENDPOINT)
+        response = await method(ENDPOINT)
         assert response.status_code == 200
         assert response.json() == []
 
 
-def make_several(client: TestClient, count: int = 10) -> list:
+@pytest.mark.asyncio
+async def test_update(client):
+    data = await make_several(client)
+    item = data[0]
+    item_id = item["id"]
+    response = await client.put(f"{ENDPOINT}/{item_id}", json={"name": "new"})
+    assert response.status_code == 200
+    json = response.json()
+    assert json["name"] != item["name"]
+    assert json["id"] == item_id
+
+
+@pytest.mark.asyncio
+async def make_several(c: AsyncClient, count: int = 10) -> list:
     data = []
     for item_id in range(1, count):
         json = {"id": item_id, "name": f"name_{item_id}"}
-        response = client.post(ENDPOINT, json=json)
+        response = await c.post(ENDPOINT, json=json)
         assert response.status_code == 200
         assert response.json() == json
         data.append(json)
-    response = client.get(ENDPOINT)
+    response = await c.get(ENDPOINT)
     assert response.status_code == 200
     assert response.json() == data
     return data
