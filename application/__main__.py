9686a164c3fdc9237aedde632659573f196717a7
Trent Dierking <deerxlord@gmail.com>
2022-04-02 15:21:47 -0500

    updates

--- application/__main__.py
+++ application/__main__.py
@@ -1,7 +1,3 @@
-import logging
-import sys
-from copy import deepcopy
-
 import uvicorn  # type: ignore
 
 from application import setup_application
@@ -10,13 +6,8 @@ from application.config.settings import Settings
 if __name__ == "__main__":
     settings = Settings()
     app = setup_application()
-    port = sys.argv[1]
     kwargs = {
         "host": "localhost",
-        "port": port,
+        "port": 8001,
     }
-    if settings.debug:
-        kwargs.update({"reload": True, "log_level": logging.DEBUG})
-    log_config = deepcopy(uvicorn.config.LOGGING_CONFIG)
-    log_config["disable_existing_loggers"] = False
-    uvicorn.run(app, log_config=log_config, **kwargs)
+    uvicorn.run(app, **kwargs)
