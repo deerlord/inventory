7da66f72364fb6bfd0886faf3e7e163fe772142a
Trent Dierking <deerxlord@gmail.com>
2022-06-24 01:10:23 -0500

    cleaned up imports

--- application/router/health.py
+++ application/router/health.py
@@ -3,8 +3,8 @@ from fastapi import Depends, HTTPException, status
 from ..lib import database
 
 
-async def check(db=Depends(database.connection)):
+async def check(db: database.SESSION = Depends(database.connection)):
     try:
-        await db.execute("SELECT 1")
+        await db.connection()
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
